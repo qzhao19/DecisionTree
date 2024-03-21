6e9c4a29f3f7f4d6f98af5143ce8ce2f569398db
qzhao19 <qi.zhao@outlook.fr>
2024-03-21 23:11:17 +0800

    add prereqs.hpp

---
+++ src/decision_tree/prereqs.hpp
@@ -0,0 +1,39 @@
+#ifndef PREREQS_HPP
+#define PREREQS_HPP
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <stdexcept>
+#include <tuple>
+#include <typeinfo>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using DataType = double;
+using LabelType = long;
+
+using HistogramType = std::vector<std::vector<DataType>>;
+
+#endif
