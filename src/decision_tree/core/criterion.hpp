c9d0cbb060c96775273aee84e51e6dbb8758ced1
qzhao19 <qi.zhao@outlook.fr>
2024-03-21 23:12:25 +0800

    add criterion.hpp

---
+++ src/decision_tree/core/criterion.hpp
@@ -0,0 +1,18 @@
+#ifndef CORE_CRITERION_HPP
+#define CORE_CRITERION_HPP
+
+#include <algorithm>
+#include <vector>
+
+namespace decisiontree {
+
+class Criterion {
+    
+
+
+
+};
+
+}
+
+#endif //CORE_CRITERION_HPP
