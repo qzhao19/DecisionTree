b99c79cc5512bb717b6f4bc742dfa36d827541bd
qzhao19 <qi.zhao@outlook.fr>
2024-03-29 21:46:43 +0800

    update

--- tests/test_sort.cpp
+++ tests/test_sort.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include "../src/decision_tree/utils/sort.hpp"
+#include "../decision_tree/utils/sort.hpp"
 
 namespace {
 
@@ -10,19 +10,13 @@ TEST(SortTest, Sort2VectorsByFirstVector){
     std::vector<double> x = {5.2, 3.3, 1.2, 0.3, 4.8, 3.1, 1.6, 0.2, 4.75};
     std::vector<std::size_t> y = {0, 0, 0, 1, 1, 1, 2, 2, 2};
 
-    sort<double, std::size_t>(x, y, 0, 5);
+    decisiontree::sort<double, std::size_t>(x, y, 0, 5);
 
     std::vector<double> expect1 = {0.3, 1.2, 3.3, 4.8, 5.2, 3.1, 1.6, 0.2, 4.75};
     std::vector<std::size_t> expect2 = {1, 0, 0, 1, 0, 1, 2, 2, 2};
 
     EXPECT_THAT(x, ::testing::ContainerEq(expect1));
     EXPECT_THAT(y, ::testing::ContainerEq(expect2));
-}
-
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
 
 }
