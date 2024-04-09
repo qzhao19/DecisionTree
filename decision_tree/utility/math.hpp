fb709eb467d556445828add50bfd240b27ece01a
qzhao19 <qi.zhao@outlook.fr>
2024-04-09 22:03:07 +0800

    rename 'utils' to 'utility'

---
+++ decision_tree/utility/math.hpp
@@ -0,0 +1,23 @@
+#ifndef UTILITY_MATH_HPP
+#define UTILITY_MATH_HPP
+
+#include "../common/prereqs.hpp"
+
+namespace decisiontree {
+
+template<typename FeatureType, typename ClassType>
+ClassType argmax(FeatureType* x, long size) {
+    ClassType max_index = 0;
+    FeatureType max_value = x[max_index];
+
+    for (unsigned long i = 0; i < size; i++) {
+        if (x[i] > max_value) {
+            max_index = i;
+            max_value = x[max_index];
+        }
+    }
+    return max_index;
+};
+
+}
+#endif // #ifndef UTILITY_RANDOM_HPP
