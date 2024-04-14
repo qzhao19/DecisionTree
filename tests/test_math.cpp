b9463786d0f54836b5fc87755d153396453de64b
qzhao19 <qi.zhao@outlook.fr>
2024-04-14 23:07:02 +0800

    add test_math.cpp

---
+++ tests/test_math.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "../decision_tree/utility/math.hpp"
+
+namespace {
+
+TEST(SortTest, Sort2VectorsByFirstVector){
+    std::vector<double> x = {5.2, 3.3, 1.2, 0.3, 4.8, 3.1, 1.6, 0.2, 4.75};
+    long max_index;
+    max_index = decisiontree::argmax<double, long>(&x[0], 9);
+    EXPECT_THAT(max_index, 0);
+};
+
+}
