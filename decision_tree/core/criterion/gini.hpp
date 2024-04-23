c803829f727826d6d09da1fe9761b9aa01b45bbe
qzhao19 <qi.zhao@outlook.fr>
2024-04-23 22:40:31 +0800

    create gini.hpp

---
+++ decision_tree/core/criterion/gini.hpp
@@ -0,0 +1,13 @@
+#ifndef CORE_CRITERION_GINI_HPP_
+#define CORE_CRITERION_GINI_HPP_
+
+#include "base.hpp"
+
+namespace decisiontree {
+
+class GiniCriterion : public BaseCriterion {
+
+};
+
+}
+#endif // CORE_CRITERION_GINI_HPP_
