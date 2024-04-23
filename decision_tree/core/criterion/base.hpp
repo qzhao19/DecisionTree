0d3ceec98c20bb1ff3e5d7834f14122932ffeb9b
qzhao19 <qi.zhao@outlook.fr>
2024-04-23 22:40:13 +0800

    create base.hpp

---
+++ decision_tree/core/criterion/base.hpp
@@ -0,0 +1,13 @@
+#ifndef CORE_CRITERION_BASE_HPP_
+#define CORE_CRITERION_BASE_HPP_
+
+#include "../../common/prereqs.hpp"
+
+namespace decisiontree {
+
+class BaseCriterion {
+
+};
+
+}
+#endif // CORE_CRITERION_BASE_HPP_
